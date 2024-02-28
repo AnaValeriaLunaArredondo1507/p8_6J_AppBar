43966090d9ae66c1226567858dfed88f47f109c2
ANA_VALERIA_LUNA_ARREDONDO <a.21308051280497@cbtis128.edu.mx>
2024-02-28 20:49:50 +0000

    actualización

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiAppBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiAppBar extends StatelessWidget {
+  const MiAppBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Valeria Luna'),
+          actions: [
+            Icon(Icons.search),
+            Icon(Icons.more_vert),
+          ],
+          centerTitle: true,
+          elevation: 8,
         ),
+        drawer: const Drawer(),
+        body: const Text('Ana Valeria Luna Arredondo'),
       ),
     );
   }
